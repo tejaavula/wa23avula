2f4c3ad0bdae0d7e4455a665bc89606e6bb94dd1
tejaswi <avula.usedu1@gmail.com>
2021-03-24 12:26:04 -0500

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var x=0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  x+=1;
+  res.send('User accesses are: '+x);
 });
 
 module.exports = router;
