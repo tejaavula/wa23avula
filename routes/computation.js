1e0f0bd15f7e2756b3792d52cafae879c146a5b0
tejaswi <avula.usedu1@gmail.com>
2021-03-24 12:40:01 -0500

    added computation file

---
+++ routes/computation.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var x;
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+    x = Math.random();
+    // res.send('hello');
+    res.write('[Math.exp()] applied to ' + `${[x]}​​​​​​​​` + ' is ' + `${[Math.exp(x)]}​​​​​​​​\n`);
+
+    res.write('[Math.expm1()] applied to ' + `${[x]}​​​​​​​​` + ' is ' + `${[Math.expm1(x)]}​​​​​​​​`);
+    res.end();
+});
+
