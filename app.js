b344c08a7e8369c2135343b369ad532ebac53d6f
tejaswi <avula.usedu1@gmail.com>
2021-03-24 21:33:04 -0500

    added bonus

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var stuffRouter = require('./routes/stuff');
 var computationRouter = require('./routes/computation');
  
 var app = express();
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/stuff', stuffRouter);
 app.use('/computation', computationRouter);
  
 // catch 404 and forward to error handler
