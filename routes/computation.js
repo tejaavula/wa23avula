26459caa116121e5fa8ee390acc4c80c69c227f5
tejaswi <avula.usedu1@gmail.com>
2021-03-24 21:30:52 -0500

    added bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-router.get('/',(req,res)=>{
-    let randomNumber=Math.floor(Math.random()*100)+1
-    let data=req.query.x
-    console.log(data)
-    res.render('computation',{
-        randomSolution:`Exponent of ${randomNumber} is ${Math.exp(randomNumber)}`,
-        querySolution:`Exponent of ${data} is ${Math.exp(data)}`
-    })
-})
+var x ;
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+ 
+x = Math.random();
+// res.send('hello');
+res.write('[Math.round()] applied to ' + `${ [x]}` + ' is ' + `${[Math.round(x)]}\n`);
+ 
+res.write('[Math.fround()] applied to ' + `${ [x]}` + ' is ' + `${[Math.fround(x)]}`);
+res.end();
+});
 
 module.exports=router
